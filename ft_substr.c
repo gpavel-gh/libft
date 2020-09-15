90159936a1af27eddd366b45b0ef09846671d972
Pavel George Iulian <gpavel@Pavels-MacBook-Air.local>
2020-09-15 18:43:23 +0200

    contiene la correccion del substr con el main

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: gpavel <gpavel@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/08 10:02:20 by gpavel            #+#    #+#             */
-/*   Updated: 2020/07/28 11:06:41 by gpavel           ###   ########.fr       */
+/*   Updated: 2020/09/15 18:39:39 by gpavel           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,25 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char			*subs;
 	unsigned int	i;
+	int				x;
 
-	if (!(subs = (char *)malloc(len + 1)) || !s)
+	if (( x = ft_strlen((char *)s) - start) < 0)
+		return(ft_strdup(""));
+	if (!s || !(subs = (char *)malloc(((int)len > x ? x : len) + 1)))
 		return (NULL);
-	if ((unsigned int)ft_strlen((char *)s) < start)
-		return (ft_strdup(""));
 	i = 0;
 	while (i < len)
 		subs[i++] = ((char *)s)[start++];
 	subs[i] = '\0';
 	return (subs);
+}
+
+int		main()
+{
+	char	*str;
+	char	*str2 = "1234";
+
+	str = ft_substr(str2, 6, 5);
+	printf("%s\n", str);
+	return (0);
 }
